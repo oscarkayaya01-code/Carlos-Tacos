9f0a9f732f7c4566f4c73e4863b26635672942be
oscarkayaya <oscarkayaya01@gmail.com>
2026-05-28 14:59:22 -0700

    minor update

--- src/main/java/com/pluralsight/models/TacoSize.java
+++ src/main/java/com/pluralsight/models/TacoSize.java
@@ -1,17 +1,23 @@
 package com.pluralsight.models;
 
 public enum TacoSize {
-    SINGLE("Single Taco"),
-    THREE_TACO("3-Taco Plate"),
-    BURRITO("Burrito");
+    SINGLE("Single Taco", 3.50),
+    THREE_TACO("3-Taco Plate", 9.00),
+    BURRITO("Burrito", 8.50);
 
     private final String displayName;
+    private final double basePrice;
 
-    TacoSize(String displayName) {
+    TacoSize(String displayName, double basePrice) {
         this.displayName = displayName;
+        this.basePrice = basePrice;
     }
 
     public String getDisplayName() {
         return displayName;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
     }
 }
