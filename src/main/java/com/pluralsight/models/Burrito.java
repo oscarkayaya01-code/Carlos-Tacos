83729727c932317e6c16bbf8ed9ca36decb52915
oscarkayaya <oscarkayaya01@gmail.com>
2026-05-29 01:49:08 -0700

    I just got done with burrito class

--- src/main/java/com/pluralsight/models/Burrito.java
+++ src/main/java/com/pluralsight/models/Burrito.java
@@ -1,4 +1,13 @@
 package com.pluralsight.models;
 
-public class Burrito {
+import java.util.ArrayList;
+
+public class Burrito extends Taco {
+
+    public Burrito(ShellType shell, String meat, int extraMeatCount,
+                   String cheese, int extraCheeseCount, ArrayList<Toppings> toppings,
+                   String sauce, ArrayList<Sides> sides, boolean covered) {
+        super(shell, TacoSize.BURRITO, meat, extraMeatCount,
+                cheese, extraCheeseCount, toppings, sauce, sides, covered);
+    }
 }
